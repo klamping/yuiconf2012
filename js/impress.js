ff0e49fcee2209f3353f562bcb417cf293834a01
Kevin Lamping <kevin.lamping@usaa.com>
2012-11-15 12:49:02 -0600

    tweaks

--- js/impress.js
+++ js/impress.js
@@ -750,18 +750,18 @@
         }, false);*/
         
         // delegated handler for clicking on step elements
-        document.addEventListener("click", function ( event ) {
-            /*var target = event.target;
+        /*document.addEventListener("click", function ( event ) {
+            var target = event.target;
             // find closest step element that is not active
             while ( !(target.classList.contains("step") && !target.classList.contains("active")) &&
                     (target !== document.documentElement) ) {
                 target = target.parentNode;
-            }*/
+            }
             
             if ( api.next() ) {
                 event.preventDefault();
             }
-        }, false);
+        }, false);*/
         
         // touch handler to detect taps on the left and right side of the screen
         // based on awesome work of @hakimel: https://github.com/hakimel/reveal.js
